479134b4db529d88cc18e584c9fecc8dd577170a
Leonard Ademi <leonard.ademi@tender-service.com>
2024-08-25 21:22:53 +0200

    DI setup

--- Task1/App.xaml.cs
+++ Task1/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Task1.Models;
 
 namespace Task1
 {
@@ -13,5 +15,29 @@ namespace Task1
     /// </summary>
     public partial class App : Application
     {
+
+        public static ServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();
+            services.AddTransient<MainWindow>();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            ServiceProvider.Dispose();
+            base.OnExit(e);
+        }
     }
 }
